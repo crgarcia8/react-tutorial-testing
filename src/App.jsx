b9133a1aa3bec8008884db1ea22b8fcd57c56286
davise070893 <davise070893@gmail.com>
2024-08-23 22:10:27 +0000

    add console.log

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(count);
   return (
     <>
       <div>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React </h1>
+      <h1>Vite + React CHANGES</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more- que noooo
+        Click on the Vite and React logos to learn more- que ghjghj
       </p>
     </>
   )
